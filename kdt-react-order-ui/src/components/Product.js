ad9778a9144995689c5b339adf4cb221ce3811f6
Yongsu Kang <dksktm15@naver.com>
2022-04-26 21:24:12 +0900

    Product App.js에서 분리

---
+++ kdt-react-order-ui/src/components/Product.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export function Product(props) {
+    const id = props.id
+    const productName = props.productName;
+    const category = props.category;
+    const price = props.price;
+
+    const handleAddBtnClicked = e => {
+        props.onAddClick(id);
+    };
+    return (
+        <>
+            <div className="col-2"><img className="img-fluid" src="https://i.imgur.com/HKOFQYa.jpeg" alt=""/></div>
+            <div className="col">
+                <div className="row text-muted">{productName}</div>
+                <div className="row">{category}</div>
+            </div>
+            <div className="col text-center price">{price}</div>
+            <div className="col text-end action">
+                <button onClick={handleAddBtnClicked} className="btn btn-small btn-outline-dark">추가</button>
+            </div>
+        </>
+    )
+}
